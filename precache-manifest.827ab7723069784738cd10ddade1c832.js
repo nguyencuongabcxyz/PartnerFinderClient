d96f21149e37baeec6fbfc9696c95fbed5e14c72
Cuong Nguyen <nguyencuongoc@gmail.com>
2019-12-22 20:54:09 +0700

    Updates

---
+++ precache-manifest.827ab7723069784738cd10ddade1c832.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3eb57284ae1810e948f47de9de3ea87",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "2210a4c32111ef781e88",
+    "url": "/my-app/static/css/2.1bfd5170.chunk.css"
+  },
+  {
+    "revision": "037c7bc52c9524cc6b70",
+    "url": "/my-app/static/css/main.2fc39d13.chunk.css"
+  },
+  {
+    "revision": "2210a4c32111ef781e88",
+    "url": "/my-app/static/js/2.13ce136d.chunk.js"
+  },
+  {
+    "revision": "037c7bc52c9524cc6b70",
+    "url": "/my-app/static/js/main.be794a88.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  }
+]);
