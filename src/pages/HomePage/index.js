2210d5ac24725676ee1b40c6f7839a222d7ce764
Cuong Nguyen <nguyencuongoc@gmail.com>
2019-12-28 14:24:15 +0700

    final version

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -81,18 +81,18 @@ class Home extends React.Component {
                     <div className="section-content row">
                         <div className="section-block col-lg-3 col-md-4 hidden">
                             <img src={process.env.PUBLIC_URL + "/images/HomePage/finding_partner.svg"} alt="finding_partner"></img>
-                            <h2>While you don’t have to use any specific library to</h2>
-                            <p>While you don’t have to use any specific library to integrate Bootstrap with React apps, it's often easier than trying to wrap the Bootstrap jQuery plugins. React Bootstrap is the most popular option that strives for complete parity with Bootstrap.</p>
+                            <h2>Finding your partner to practice English with</h2>
+                            <p>Partner finder helps you to find people having passion of learning English like you. They will help you improve your skills and you will do that in return</p>
                         </div>
                         <div className="section-block col-lg-3 col-md-4 hidden">
                             <img src={process.env.PUBLIC_URL + "/images/HomePage/asking.svg"} alt="asking"></img>
-                            <h2>While you don’t have to use any specific library to</h2>
-                            <p>While you don’t have to use any specific library to integrate Bootstrap with React apps, it's often easier than trying to wrap the Bootstrap jQuery plugins. React Bootstrap is the most popular option that strives for complete parity with Bootstrap. </p>
+                            <h2>Asking question hard to solve</h2>
+                            <p>Partner finder helps you to receive answers of your questions from thousands of other people that you can not deal with. Anytime when you study alone.</p>
                         </div>
                         <div className="section-block col-lg-3 col-md-4 hidden">
                             <img src={process.env.PUBLIC_URL + "/images/HomePage/feedback.svg"} alt="feedback"></img>
-                            <h2>While you don’t have to use any specific library to</h2>
-                            <p>While you don’t have to use any specific library to integrate Bootstrap with React apps, it's often easier than trying to wrap the Bootstrap jQuery plugins. React Bootstrap is the most popular option that strives for complete parity with Bootstrap. </p>
+                            <h2>Getting feedback from other people</h2>
+                            <p>Partner finder helps you to receive feedbacks for your English practice from thousands of other people.It helps you to improve speaking and writing skills</p>
                         </div>
                     </div>
                     <DownButton link="section3" textColor="#4f4f4f" headContent="See how" tailContent="works" />
@@ -104,28 +104,28 @@ class Home extends React.Component {
                         <div className="guide-block block-left">
                             <img alt="guide_image" src={process.env.PUBLIC_URL + "/images/HomePage/exam_guide.svg"}></img>
                             <div>
-                            <h2><span>1</span>While you don’t have to use any specific library to integrate Bootstrap with React apps</h2>
-                                <p>While you don’t have to use any specific library to integrate Bootstrap with React apps, it's often easier than trying to wrap the Bootstrap jQuery plugins. React Bootstrap is the most popular option that strives for complete parity with Bootstrap. reactstrap is also a good choice</p>
+                            <h2><span>1</span>Take a test and PartnerFinder will help you to get your english level</h2>
+                                <p>After you sign up and log into PartnerFinder, you can choose to have a test to get your level so that you can find your ideal partner.</p>
                             </div>
                         </div>
                         <div className="guide-block block-right">
                             <div>
-                            <h2><span>2</span>While you don’t have to use any specific library to integrate Bootstrap with React apps</h2>
-                                <p>While you don’t have to use any specific library to integrate Bootstrap with React apps, it's often easier than trying to wrap the Bootstrap jQuery plugins. React Bootstrap is the most popular option that strives for complete parity with Bootstrap. reactstrap is also a good choice</p>
+                            <h2><span>2</span>Update your information</h2>
+                                <p>After log into PartnerFinder you can update you profile with a introduction video so that other people can find you when looking at your profile</p>
                             </div>
                             <img alt="guide_image" src={process.env.PUBLIC_URL + "/images/HomePage/fill_info_guide.svg"}></img>
                         </div>
                         <div className="guide-block block-left">
                             <img alt="guide_image" src={process.env.PUBLIC_URL + "/images/HomePage/search_guide.svg"}></img>
                             <div>
-                            <h2><span>3</span>While you don’t have to use any specific library to integrate Bootstrap with React apps</h2>
-                                <p>While you don’t have to use any specific library to integrate Bootstrap with React apps, it's often easier than trying to wrap the Bootstrap jQuery plugins. React Bootstrap is the most popular option that strives for complete parity with Bootstrap. reactstrap is also a good choice</p>
+                            <h2><span>3</span>Looking for your partner</h2>
+                                <p>You can find your ideal partners when looking at other people of PartnerFinder site.When you have partners, you can interact with them in many way</p>
                             </div>
                         </div>
                         <div className="guide-block block-right">
                             <div>
-                                <h2><span>4</span>While you don’t have to use any specific library to integrate Bootstrap with React apps</h2>
-                                <p>While you don’t have to use any specific library to integrate Bootstrap with React apps, it's often easier than trying to wrap the Bootstrap jQuery plugins. React Bootstrap is the most popular option that strives for complete parity with Bootstrap. reactstrap is also a good choice</p>
+                                <h2><span>4</span>Asking questions and feedbacks</h2>
+                                <p>You can ask questions for the problems that you can not solve and ask to get feedbacks from other user when you study and practice English alone </p>
                             </div>
                             <img alt="guide_image" src={process.env.PUBLIC_URL + "/images/HomePage/question_guide.svg"}></img>
                         </div>
